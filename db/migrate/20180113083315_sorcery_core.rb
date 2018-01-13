bb9e35bae34a6052bedecfc0412e080f2941cfc1
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-13 17:43:10 +0900

    create table users

--- db/migrate/20180113083315_sorcery_core.rb
+++ db/migrate/20180113083315_sorcery_core.rb
@@ -1,11 +1,13 @@
 class SorceryCore < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :email,            :null => false
-      t.string :crypted_password
-      t.string :salt
-
-      t.timestamps                :null => false
+      t.string :email, null: false
+      t.string :name, null: false
+      t.string :crypted_password, null: false
+      t.string :salt, null: false
+      t.integer :role, null: false, default: 0
+      t.integer :superior_id, index: true, foreign_key: true
+      t.timestamps null: false
     end
 
     add_index :users, :email, unique: true
