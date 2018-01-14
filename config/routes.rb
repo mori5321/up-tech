fd187040ea8fee53e36e8273a4b79fe2daed4c23
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-14 13:09:24 +0900

    menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:create]
+  resources :users, only: [:new, :create]
   resources :user_sessions, only: [:create]
+  resources :menu, only: :index
+
   resources :tasks, only: [:new, :create, :show] do
     collection do
       get 'monthly'
@@ -10,7 +12,8 @@ Rails.application.routes.draw do
     end
   end
 
-  get 'sign_up' => 'users#new', as: :sign_up
+  # adminがUserを作成する仕様かと判断しこめんとアウト
+  # get 'sign_up' => 'users#new', as: :sign_up
   get 'sign_in' => 'user_sessions#new', as: :sign_in
   post 'logout' => 'user_sessions#destroy', as: :sign_out
 end
