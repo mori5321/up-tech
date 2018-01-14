c19fecebe9af088321e6732fa19bc72d2d230ee2
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-14 12:50:54 +0900

    create tasks controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,16 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def monthly
+  end
+
+  def weekly
+  end
+
+  def daily
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
   resources :user_sessions, only: [:create]
+  resources :tasks, only: [:new, :create, :show] do
+    collection do
+      get 'monthly'
+      get 'weekly'
+      get 'daily'
+    end
+  end
 
   get 'sign_up' => 'users#new', as: :sign_up
   get 'sign_in' => 'user_sessions#new', as: :sign_in
