5adc6cac4de81e8330f0c7cb36f2e01757d086c4
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-16 22:07:40 +0900

    add datetime test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -4,4 +4,18 @@ describe Task do
   it 'has a valid factory' do
     expect(build(:task)).to be_valid
   end
+
+  describe "datetime" do
+    it "is valid when start_datetime is less than finish_datetime" do
+      task = build(:task, start_time: "10:00", finish_time: "10:01")
+      task.valid?
+      expect(task).to be_valid
+    end
+
+    it "is invalid when start_datetime is greater than finish datetime" do
+      task = build(:task, start_time: "10:00", finish_time: "09:59")
+      task.valid?
+      expect(task.errors[:start_date]).to include("をfinish_dateより前に入力してください")
+    end
+  end
 end
