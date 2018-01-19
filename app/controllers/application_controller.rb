7bfed40cd395d3b121c0e1db8b4d5c684adce2a3
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-19 16:14:50 +0900

    change path of redirection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   rescue_from Banken::NotAuthorizedError, with: :user_not_authorized
 
   private
+  def not_authenticated
+    redirect_to :sign_in
+  end
+
   def user_not_authorized(exception)
     loyalty_name = exception.loyalty.class.to_s.underscore
 
