c4ada4e0a8b869e6e8463e96179e2726b77baefc
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-19 13:10:36 +0900

    spec for task model

--- spec/factories/reports.rb
+++ spec/factories/reports.rb
@@ -2,5 +2,22 @@
 
 FactoryGirl.define do
   factory :report do
+    date "2018-10-01"
+    start_time "11:00"
+    finish_time "11:30"
+
+    factory :weekly_report do
+      week "2018W01"
+      date nil
+      start_time nil
+      finish_time nil
+    end
+
+    factory :monthly_report do
+      month "2018-01"
+      date nil
+      start_time nil
+      finish_time nil
+    end
   end
 end
