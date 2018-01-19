9b3a275dc526e890e79832df018be4c442211b39
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-19 15:03:26 +0900

    set redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :require_login
+
+  rescue_from Banken::NotAuthorizedError, with: :user_not_authorized
+
+  private
+  def user_not_authorized(exception)
+    loyalty_name = exception.loyalty.class.to_s.underscore
+
+    flash[:error] = t "#{loyalty_name}.#{exception.query}", scope: "banken", default: :default
+    redirect_to(request.referrer || root_path)
+  end
 end
