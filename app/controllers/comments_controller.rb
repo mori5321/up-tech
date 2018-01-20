89431590d7d7e4ab6fbb749d1b37e2e07b5e9cc0
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-20 13:45:56 +0900

    pre-development form for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,11 @@ class CommentsController < ApplicationController
   include Banken
 
   def new
+    report = current_user.subordinate_users_reports.first
+    @report = report.decorate
+    task = @report.task
+    @task = task.decorate
+    @comment = @report.comments.build
   end
 
   def create
