d0eacf899bf8be484bfe99d4d3a01f28b0444469
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-21 17:58:49 +0900

    modify migration

--- db/migrate/20180121082000_create_stamps.rb
+++ db/migrate/20180121082000_create_stamps.rb
@@ -2,7 +2,7 @@ class CreateStamps < ActiveRecord::Migration[5.1]
   def change
     create_table :stamps do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :report_id, null: false, index: true, foreign_key: true
+      t.references :report, null: false, foreign_key: true, index: true
       t.integer :kind
       t.timestamps
     end
