28e3b331764c124b0d18164d1cb5dfc5a1a04d6d
西森大介 <nishimori.daisuke@di-v.co.jp>
2018-01-22 01:11:31 +0900

    server side for axis

--- app/controllers/stamps_controller.rb
+++ app/controllers/stamps_controller.rb
@@ -1,5 +1,12 @@
 class StampsController < ApplicationController
   def create
-    
+    stamp = Stamp.new(kind: params[:kind], user_id: current_user.id, report_id: params[:report_id])
+    stamp.save
+
+    respond_to do |format|
+      format.html { render text: "hello" }
+      format.json
+    end
   end
+
 end
